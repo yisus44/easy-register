c03a768c07effa5521035859a6e85b1e718babc6
yisus44 <jesusadrian1953.1@gmail.com>
2022-06-12 19:25:29 -0500

    added create class screen

--- lib/screens/create_class_screen.dart
+++ lib/screens/create_class_screen.dart
@@ -1,27 +1,131 @@
 import 'package:easy_register/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class CreateClassScreen extends StatelessWidget {
+class CreateClassScreen extends StatefulWidget {
+  @override
+  State<CreateClassScreen> createState() => _CreateClassScreenState();
+}
+
+class _CreateClassScreenState extends State<CreateClassScreen> {
+  List<bool> _selections = List.generate(2, (_) => false);
+
+  void selectStudents() {}
+
+  void selectHours() {}
+
+  Widget _buildPortraitMode() {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        const AuthenticationFields(
+            typeText: true, titleText: 'Nombre de la clase'),
+        GestureDetector(
+          child: const AuthenticationLabel("Alumnos"),
+          onTap: selectStudents,
+        ),
+        GestureDetector(
+          child: const AuthenticationLabel("Horarios"),
+          onTap: selectStudents,
+        ),
+        Row(
+          children: [
+            const AuthenticationLabel("Calificacion basada en"),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ToggleButtons(
+                children: <Widget>[Text("10%"), Text("100%")],
+                isSelected: _selections,
+                onPressed: (int index) {
+                  setState(() {
+                    if (index == 0) {
+                      _selections[index] = !_selections[index];
+                      _selections[index + 1] = !_selections[index];
+                    } else {
+                      _selections[index] = !_selections[index];
+                      _selections[index - 1] = !_selections[index];
+                    }
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+        const AuthenticationLabel("Encuadre de asignaciones"),
+        const CardSlider(carouselName: "Rubros", cardsCount: 2),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            AuthenticationButton(buttonText: "Crear"),
+            AuthenticationButton(buttonText: "Cancelar")
+          ],
+        )
+      ],
+    ));
+  }
+
+  Widget _buildLandscapeContent() {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        const AuthenticationFields(
+            typeText: true, titleText: 'Nombre de la clase'),
+        GestureDetector(
+          child: const AuthenticationLabel("Alumnos"),
+          onTap: selectStudents,
+        ),
+        GestureDetector(
+          child: const AuthenticationLabel("Horarios"),
+          onTap: selectStudents,
+        ),
+        Row(
+          children: [
+            const AuthenticationLabel("Calificacion basada en"),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: ToggleButtons(
+                children: <Widget>[Text("10%"), Text("100%")],
+                isSelected: _selections,
+                onPressed: (int index) {
+                  setState(() {
+                    if (index == 0) {
+                      _selections[index] = !_selections[index];
+                      _selections[index + 1] = !_selections[index];
+                    } else {
+                      _selections[index] = !_selections[index];
+                      _selections[index - 1] = !_selections[index];
+                    }
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+        const AuthenticationLabel("Encuadre de asignaciones"),
+        const CardSlider(carouselName: "Rubros", cardsCount: 2),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            AuthenticationButton(buttonText: "Crear"),
+            AuthenticationButton(buttonText: "Cancelar")
+          ],
+        )
+      ],
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: Container(
-          child: 
-            Image.asset('assets/logo_chico (2).png'),
-            margin: const EdgeInsets.only(left: 5),
-            //padding: EdgeInsets.only(left: 10),
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              
-            ],
-          ) 
-        ),
-    );
+        appBar: const CustomAppBar(
+            "assets/logo_small_transparent.png", "Crear clase"),
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return Center(
+              child: orientation == Orientation.portrait
+                  ? _buildPortraitMode()
+                  : _buildLandscapeContent(),
+            );
+          },
+        ));
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,23 +10,30 @@ class HomeScreen extends StatelessWidget {
           "assets/logo_small_transparent.png", "Bienvenido fulanito"),
       body: SingleChildScrollView(
           child: Column(
-            children: [
-              const CardSlider(carouselName: 'Clases', cardsCount: 10,),
-              const CardSlider(carouselName: 'Presentaciones easer', cardsCount: 10,),
-              const CardSlider(carouselName: 'Material', cardsCount: 10,),
-              ElevatedButton(
+        children: [
+          const CardSlider(
+            carouselName: 'Clases',
+            cardsCount: 10,
+          ),
+          const CardSlider(
+            carouselName: 'Presentaciones easer',
+            cardsCount: 10,
+          ),
+          const CardSlider(
+            carouselName: 'Material',
+            cardsCount: 10,
+          ),
+          ElevatedButton(
               onPressed: () async {
-                final ClassService service = ClassService("https://gentle-mountain-69254.herokuapp.com/api/v1");
+                final ClassService service = ClassService(
+                    "https://gentle-mountain-69254.herokuapp.com/api/v1");
                 final response = await service.getAllClasses();
                 int sum = 3 + 2;
                 sum += 2;
               },
-              child: const Text("hola esquizo")
-              )
-            ],
-          ) 
-      ),
-      
+              child: const Text("hola esquizo"))
+        ],
+      )),
     );
   }
 }
