9218e5d09bf98d74a55fcdcab75ad0b6b83df484
sarahi salazar <a20110332@ceti.mx>
2022-06-15 14:02:08 -0500

    tableLayout

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../screens/screens.dart';
 
 class AppRoutes {
-  static const initialRoute = 'material';
+  static const initialRoute = 'grades';
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'login'      : (_) => LoginScreen(),
@@ -11,7 +11,8 @@ class AppRoutes {
     'home'       : (_) => HomeScreen(1),
     'createClass': (_) => const CreateClassScreen(teacherId: 1),
     'student'    : (_) => StudentScreen(),
-    'material'   : (_)=> const MaterialScreen(countColumn: 1,),
+    'material'   : (_) => const MaterialScreen(countColumn: 1,),
+    'grades'     : (_) => const GradesScreen()
   };
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
