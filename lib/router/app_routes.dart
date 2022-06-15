e1baa68a6c180b8027ca80df363b34ad4252b376
yisus44 <jesusadrian1953.1@gmail.com>
2022-06-15 14:37:37 -0500

    changed count columb

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -5,14 +5,16 @@ class AppRoutes {
   static const initialRoute = 'grades';
 
   static Map<String, Widget Function(BuildContext)> routes = {
-    'login'      : (_) => LoginScreen(),
-    'register'   : (_) => RegisterScreen(),
-    'class'      : (_) => ClassScreen(),
-    'home'       : (_) => HomeScreen(1),
+    'login': (_) => LoginScreen(),
+    'register': (_) => RegisterScreen(),
+    'class': (_) => ClassScreen(),
+    'home': (_) => HomeScreen(1),
     'createClass': (_) => const CreateClassScreen(teacherId: 1),
-    'student'    : (_) => StudentScreen(),
-    'grades'     : (_) => const GradesScreen(),
-    'material'   : (_)=> const MaterialScreen(countColumn: 1,),
+    'student': (_) => StudentScreen(),
+    'grades': (_) => const GradesScreen(),
+    'material': (_) => const MaterialScreen(
+          countColumn: 3,
+        ),
     'test': (_) => TestScreen()
   };
 
